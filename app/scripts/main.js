490b6744e182b587bccc6d79171ea7480b2f2c04
heleneshaikh <helene.shaikh@gmail.com>
2016-12-06 18:58:30 +0100

    add restrictions, list and  styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,17 +17,34 @@ var ViewModel = function () {
       position: {lat: locations[i][0], lng: locations[i][1]},
       map: map,
       icon: {
-        url: ('images/Flower.svg'),
+        url: ('images/flower.svg'),
         scaledSize: new google.maps.Size(45, 45)
-      }
+      },
+      animation: google.maps.Animation.DROP,
     });
+
   }
+  //restrict autocomplete to Croatia
+  var options= {
+    componentRestrictions: {country: 'hr'}
+  };
+  var input = document.getElementById("country");
+  var autoComplete = new google.maps.places.Autocomplete(input, options);
+
 };
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: 44.2302337, lng: 15.8964747},
-    zoom: 8
+    center: {lat: 44.4464391, lng: 14.936299},
+    zoom: 8,
+    styles: [
+      {
+        featureType: "water",
+        stylers : [
+          {color: "#1FCEC9"}
+        ]
+      }
+    ]
   });
 
   ko.applyBindings(new ViewModel());
