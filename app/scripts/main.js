ed6579b82884dc4505c8d2bc07eeb748680c4395
heleneshaikh <helene.shaikh@gmail.com>
2016-12-11 14:26:18 +0100

    edit array

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,44 +2,61 @@
  The waterfall and nature icons are licensed by Flaticon Basic License
  */
 
-//MODEL of locations[latitude, longitude, name, image file, image icon]
+//MODEL
 var locations = [
-  [44.8654, 15.5820, "Plitvice National Park", "images/plitvice.jpg", "images/waterfall-icon.svg", "images/search-icon.svg"], //flat
-  [43.8666, 15.9725, "Krka National Park", "images/krka.jpg", "images/waterfall-icon.svg", "images/search-icon.svg"],
-  [43.3252778, 17.046978, "Biokovo", "images/biokovo.jpg", "images/mountain.svg", "images/search-icon.svg"],
-  [43.2595, 17.1025, "vrata biokova", "images/cheeseburger.jpg", "images/hamburger.svg", "images/search-icon.svg"],
-  [43.1561, 17.6080, "Kravica", "images/kravice.jpg", "images/waterfall-icon.svg"],
-  [43.2622, 16.6541, "Island Bol", "", "images/nature.svg"],
-  [45.0809, 14.5926, "Krk Island", "", "images/nature.svg"],
-  [43.8707, 15.2324, "Kornati National Park", "", "images/nature.svg"]
+  {lat: 44.8654, lng: 15.5820, title: "Plitvice Lakes National Park", marker: "images/waterfall-icon.svg"},
+  {lat: 43.8666, lng: 15.9725, title: "Krka National Park", marker: "images/waterfall-icon.svg"},
+  {lat: 43.3252778, lng: 17.046978, title: "Biokovo", marker: "images/mountain.svg"},
+  {lat: 43.1561, lng: 17.6080, title: "Kravica", marker: "images/waterfall-icon.svg"},
+  {lat: 43.2622, lng: 16.6541, title: "Bol, Croatia", marker: "images/nature.svg"},
+  {lat: 45.0809, lng: 14.5926, title: "Krk", marker: "images/nature.svg"},
+  {lat: 43.8707, lng: 15.2324, title: "Kornati", marker: "images/nature.svg"}
 ];
 
 var map;
 
 var ViewModel = function () {
-  var marker, i, content;
+  var marker, i;
   var infowindow = new google.maps.InfoWindow();
 
   //CREATE MARKERS
   for (i = 0; i < locations.length; i++) {
     marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+      position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
       map: map,
       icon: {
-        url: (locations[i][4]),
+        url: locations[i].marker,
         scaledSize: new google.maps.Size(30, 30)
       },
       animation: google.maps.Animation.DROP,
+      title: locations[i].title
     });
 
     //ADD INFOWINDOW CONTENT ON CLICK
-    content = '<div><p>' + locations[i][2] + '</p><img src=" ' + locations[i][3] + '"></div>';
-    google.maps.event.addListener(marker, 'click', (function (marker, i, content) {
+    google.maps.event.addListener(marker, 'click', (function (marker) {
+      var content, image, url;
       return function () {
-        infowindow.setContent(content);
-        infowindow.open(map, marker);
+        $.ajax({
+          url: "https://en.wikipedia.org/w/api.php?action=query&indexpageids=1&format=json&prop=pageimages|info&pithumbsize=250&inprop=url&utf8=1&titles=" + marker.title,
+          dataType: "jsonp"
+        }).done(function (response) {
+          var result = response.query.pages[response.query.pageids[0]];
+          if (response.query.pageids < 0) {
+            content = '<div><h2>' + marker.title + '</h2><p>data could not be loaded</p></div>';
+          } else {
+            image = result.thumbnail.source;
+            url = result.fullurl;
+            content = '<div><img src="' + image + '" alt=""><h2>' + marker.title + '</h2><p><a href="' + url + '" target="_blank">Read More</a></p></div>';
+          }
+          infowindow.setContent(content);
+          infowindow.open(map, marker);
+        }).error(function () {
+          content = '<div><h2>' + marker.title + '</h2><p>data could not be loaded</p></div>';
+          infowindow.setContent(content);
+          infowindow.open(map, marker);
+        });
       };
-    })(marker, i, content));
+    })(marker));
 
     //MAKE MARKER BOUNCE
     marker.addListener("mouseover", function () {
@@ -60,6 +77,7 @@ var ViewModel = function () {
   var options = {
     componentRestrictions: {country: 'hr'}
   };
+
   var input = document.getElementById("input");
   new google.maps.places.Autocomplete(input, options);
 
@@ -74,10 +92,10 @@ var ViewModel = function () {
     if (address == '') {
       console.log("you must enter an address");
     } else {
-      geocoder.geocode({address: address,}, function (results, status) {
+      geocoder.geocode({address: address}, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
           map.setCenter(results[0].geometry.location);
-          console.log("geo" + results[0].geometry.location);
+          // console.log("geo" + results[0].geometry.location);
           map.setZoom(12);
         } else {
           console.log("location not found");
@@ -88,29 +106,34 @@ var ViewModel = function () {
 
   //FILTER
   var self = this;
-  self.filterItems = ko.observable(); //input value
-  var titles = [];
+  self.inputFilter = ko.observable(); //input value
+  var locArray = [];
   for (var j = 0; j < locations.length; j++) {
-    titles.push(locations[j][2]);
+    locArray.push(locations[j].title);
   }
-  self.allLocations = ko.observableArray(titles); //complete array
+
+  //todo lower case
+  self.allLocations = ko.observableArray(locArray); //complete array of objects
   self.filteredItems = ko.computed(function () {
-    var filterItems = self.filterItems();
-    if (!filterItems) {
+    var inputFilter = self.inputFilter();
+    if (!inputFilter) {
       return self.allLocations();
     } else {
-      return self.allLocations().filter(function (i) {
-        return i.indexOf(filterItems) >= 0;
+      return ko.utils.arrayFilter(self.allLocations(), function (i) {
+        return i.toLowerCase().indexOf(inputFilter) >= 0;
       });
     }
   });
 };
+
+
 
 //INITIALIZE THE MAP
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 44.288834, lng: 17.0725821},
     zoom: 8,
+    icon: "images/mountain.svg"
   });
   ko.applyBindings(new ViewModel());
 }
